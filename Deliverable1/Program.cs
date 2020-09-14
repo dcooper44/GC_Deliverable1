7b86572073ee514802107c2112cb7d2a25a28c0d
Dylan <dylancooper44@gmail.com>
2020-09-14 17:24:49 -0400

    First Commit for Deliverable 1

---
+++ Deliverable1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goodPassword = false;
+
+            while (goodPassword == false)
+            {
+
+                Console.WriteLine("Hello, Please Enter a Valid Password. Passwords must contain at least one of the following characters: A lowercase letter, an uppercase letter, and an explination mark. Passwords must be between 7-12 characters long");
+
+                string password = Console.ReadLine();
+
+
+                if (password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit) && password.Contains("!")
+                    && password.Count() > 6 && password.Count()< 13 )
+                {
+                    goodPassword = true;
+                    Console.WriteLine("Password Valid and Accepted");
+                }
+                else { Console.WriteLine("Error"); }
+
+            }
+
+
+        }
+    }
+}
+
